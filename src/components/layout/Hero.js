e17791f7b82dd9e5217c200311d23a9b6859112f
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-01-02 22:49:16 -0800

    basic header layout

---
+++ src/components/layout/Hero.js
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <section>
+            <h1>Everything is better with a Pizza</h1>
+            <p>
+                Pizza is the missing piece that makes every day complete, a simple yet delicious hoy in life
+            </p>
+        </section>
+    )
+}
